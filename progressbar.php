2b2f66a3686113b99674b9e6047d4f13ad979acb
Jacob <jgsteves@uwaterloo.ca>
2017-05-16 21:05:05 -0400

    Added frontend display file

---
+++ progressbar.php
@@ -0,0 +1,88 @@
+<html>
+	<h3>Programming Language Progress Bars</h3>
+	<div id="skillbars">
+	  <div class="skillbar clearfix " data-percent="95%">
+	    <div class="skillbar-title" style="background: #d35400;"><span>HTML5</span></div>
+	    <div class="skillbar-bar" style="background: #e67e22;">    </div>
+	    <div class="skill-bar-percent">95%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="85%">
+	    <div class="skillbar-title" style="background: #443063;"><span>CSS3</span></div>
+	    <div class="skillbar-bar" style="background: #563d7c;"></div>
+	    <div class="skill-bar-percent">85%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="90%">
+	    <div class="skillbar-title" style="background: #2c3e50;"><span>jQuery</span></div>
+	    <div class="skillbar-bar" style="background: #2c3e50;"></div>
+	    <div class="skill-bar-percent">90%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="95%">
+	    <div class="skillbar-title" style="background: #3f4a77;"><span>PHP</span></div>
+	    <div class="skillbar-bar" style="background: #4F5D95;"></div>
+	    <div class="skill-bar-percent">95%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="95%">
+	    <div class="skillbar-title" style="background: #a89c3e;"><span>Javascript</span></div>
+	    <div class="skillbar-bar" style="background: #f1e05a;"></div>
+	    <div class="skill-bar-percent">95%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="85%">
+	    <div class="skillbar-title" style="background: #aeacac;"><span>SQL</span></div>
+	    <div class="skillbar-bar" style="background: #dad8d8;"></div>
+	    <div class="skill-bar-percent">85%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="85%">
+	    <div class="skillbar-title" style="background: #124e8c;"><span>React</span></div>
+	    <div class="skillbar-bar" style="background: #4288d0;"></div>
+	    <div class="skill-bar-percent">85%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="80%">
+	    <div class="skillbar-title" style="background: #124e8c;"><span>React-Native</span></div>
+	    <div class="skillbar-bar" style="background: #4288d0;"></div>
+	    <div class="skill-bar-percent">80%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="90%">
+	    <div class="skillbar-title" style="background: #444444;"><span>C</span></div>
+	    <div class="skillbar-bar" style="background: #555555;"></div>
+	    <div class="skill-bar-percent">90%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="75%">
+	    <div class="skillbar-title" style="background: #126b00;"><span>C#</span></div>
+	    <div class="skillbar-bar" style="background: #178600;"></div>
+	    <div class="skill-bar-percent">75%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="75%">
+	    <div class="skillbar-title" style="background: #5351c8;"><span>Objective-C</span></div>
+	    <div class="skillbar-bar" style="background: #6866FB;"></div>
+	    <div class="skill-bar-percent">75%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="80%">
+	    <div class="skillbar-title" style="background: #171764;"><span>Scheme</span></div>
+	    <div class="skillbar-bar" style="background: #22228f;"></div>
+	    <div class="skill-bar-percent">80%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="65%">
+	    <div class="skillbar-title" style="background: #2a5b84;"><span>Python</span></div>
+	    <div class="skillbar-bar" style="background: #3572A5;"></div>
+	    <div class="skill-bar-percent">65%</div>
+	  </div> <!-- End Skill Bar -->
+
+	  <div class="skillbar clearfix " data-percent="70%">
+	    <div class="skillbar-title" style="background: #8c5b14;"><span>Java</span></div>
+	    <div class="skillbar-bar" style="background: #b07219;"></div>
+	    <div class="skill-bar-percent">70%</div>
+	  </div> <!-- End Skill Bar -->
+	</div>
+</html>
