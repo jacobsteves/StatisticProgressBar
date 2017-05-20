c7ca90e3e8afeb09065662f9eedf82d9b92aef94
Jacob <jgsteves@uwaterloo.ca>
2017-05-20 17:57:15 -0400

    Updated documentation and added tab switching

--- skillbar.js
+++ skillbar.js
@@ -1,7 +1,22 @@
 jQuery(document).ready(function(){
+	// This function gets simply animates the skillbars to slide out to
+	//     its corresponding percentage
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
 			width:jQuery(this).attr('data-percent')
 		},6000);
 	});
+	
+	// This function gets called whenever a tab gets clicked
+	//     - switches current viewed tab to the tab corresponding
+	//       to the tab clicked
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 });
